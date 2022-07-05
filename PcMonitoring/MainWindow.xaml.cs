1d4a1922e346fc841e1d75eec7f33ec340becf29
arthur <arthurfoadjo@yahoo.com>
2022-07-06 00:43:01 +0200

    First modul

--- PcMonitoring/MainWindow.xaml.cs
+++ PcMonitoring/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PcMonitoring
 {
@@ -30,6 +32,54 @@ namespace PcMonitoring
         public MainWindow()
         {
             InitializeComponent();
+
+            // Get PC´s infos
+            GetAllSystemInfos();
+
+            // Timer for real time updates
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.75);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Timer function to refresh the screen 2 times per second
+        /// </summary>
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            // Update of CPU´s infos
+            CPULabel.Content = RefreshCPUInfos();
+        }
+
+        public string RefreshCPUInfos()
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter();
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+            dynamic firstVal = cpuCounter.NextValue(); // This variable will always be 0
+            System.Threading.Thread.Sleep(75);
+            dynamic val = cpuCounter.NextValue();// Thrue value
+
+            // Rotate the image
+            RotateTransform rotaateTransform = new RotateTransform((val*2.7f) - 90);
+            pointerImage.RenderTransform = rotaateTransform;
+
+            decimal roundVal = Convert.ToDecimal(val);
+            roundVal = Math.Round(roundVal, 2);
+
+            return roundVal +" %";
+        }
+
+        public void GetAllSystemInfos()
+        {
+            SystemInfos si = new SystemInfos();
+            OSNameLabel.Content = si.GetOSInfos("os");
+            OSArchitectureNameLabel.Content = si.GetOSInfos("architecture");
+            processorNameLabel.Content = si.GetCPUInfos();
+            GPUNameLabel.Content = si.GetGPUInfos();
         }
 
         private void infoMessageLabel_MouseDown(object sender, MouseButtonEventArgs e)
---
+++ PcMonitoring/SystemInfos.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Win32;
+
+namespace PcMonitoring
+{
+    class SystemInfos
+    {
+        /// <summary>
+        /// Get the OS´s infos
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public string GetOSInfos(string param)
+        {
+            // The ManagementObjectSearcher´s class allows access to system information
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_operatingSystem");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                switch (param)
+                {
+                    case "os":
+                        return mo["Caption"].ToString();
+                    case "architecture":
+                        return mo["OSArchitecture"].ToString();
+                    case "osversion":
+                        return mo["CSDVersion"].ToString();
+                }
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// Get the CPU´s infos
+        /// </summary>
+        /// <returns></returns>
+        public string GetCPUInfos()
+        {
+            RegistryKey processor_name = Registry.LocalMachine.OpenSubKey(@"Hardware\Description\System\CentralProcessor\0", RegistryKeyPermissionCheck.ReadSubTree);
+
+            if (processor_name != null)
+            {
+                return processor_name.GetValue("ProcessorNameString").ToString();
+            }
+
+            return "";
+        }
+
+        /// <summary>
+        /// Get Graphics Processing Unit´s infos
+        /// </summary>
+        /// <returns></returns>
+        public string GetGPUInfos()
+        {
+            using (var searcher = new ManagementObjectSearcher("select * from win32_VideoController"))
+            {
+                foreach (ManagementObject managementObject in searcher.Get())
+                {
+                    Console.WriteLine("Name - " + managementObject["Name"]);
+
+                    return managementObject["Name"].ToString() + " (Driver´s version : " + managementObject["DriverVersion"].ToString() + ")";
+                }
+            }
+            return "";
+        }
+    }
+}
